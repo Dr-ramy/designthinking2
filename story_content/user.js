2499d936261fe72d022a6365a1f781c9e001d4b8
Dr-ramy <133519124+Dr-ramy@users.noreply.github.com>
2023-06-06 20:57:24 +0300

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,405 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "662G7eUvoCL":
+        Script1();
+        break;
+      case "6gVDVJT1DLU":
+        Script2();
+        break;
+      case "6bmhCDAAuZR":
+        Script3();
+        break;
+      case "5t29You315t":
+        Script4();
+        break;
+      case "5i5AJRNnYbu":
+        Script5();
+        break;
+      case "5m56f0VfcdF":
+        Script6();
+        break;
+      case "651cTHKNZIP":
+        Script7();
+        break;
+      case "6HUB61nBp4E":
+        Script8();
+        break;
+      case "5YOkpyvdnrS":
+        Script9();
+        break;
+      case "5wBuTrnRuwH":
+        Script10();
+        break;
+      case "6ATeCyHD1GD":
+        Script11();
+        break;
+      case "6flL0NbGVDZ":
+        Script12();
+        break;
+      case "6hjNSdziZ7l":
+        Script13();
+        break;
+  }
+}
+
+function Script1()
+{
+  // JavaScript Document// JavaScript Documentconst 
+url = "https://script.google.com/macros/s/AKfycbzrM3k8moYjvK8FLjFs2GWZ_RiYvi4T4TQiMr_QbiKgkorNcgccsSseUDmbCRPlbHoh/exec";
+const player = GetPlayer();
+let uid = player.GetVar("id"); 
+let uname = player.GetVar("username");
+let ucell = player.GetVar("cell");
+let umail = player.GetVar("mail");
+let umajor = player.GetVar("major");
+let uheight = player.GetVar("height");
+let uweight = player.GetVar("weight"); 
+let uage = player.GetVar("age");
+let umeasure = player.GetVar("measure");
+let uvid = player.GetVar("vid");
+let uactiv = player.GetVar("activ");
+let uwork = player.GetVar("work");
+let uslide = player.GetVar("slide");
+fetch(url,{
+  method: 'POST',
+  mode: 'no-cors',
+  cache: 'no-cache',
+  headers: {'Content-Type': 'application/json'},
+  redirect: 'follow',
+  body: JSON.stringify({id:uid,username:uname,cell:ucell,mail:umail,major:umajor,height:uheight,weight:uweight,age:uage,measure:umeasure,vid:uvid,activ:uactiv,work:uwork,slide:uslide})
+});
+}
+
+function Script2()
+{
+  // JavaScript Document// JavaScript Documentconst 
+url = "https://script.google.com/macros/s/AKfycbzrM3k8moYjvK8FLjFs2GWZ_RiYvi4T4TQiMr_QbiKgkorNcgccsSseUDmbCRPlbHoh/exec";
+const player = GetPlayer();
+let uid = player.GetVar("id"); 
+let uname = player.GetVar("username");
+let ucell = player.GetVar("cell");
+let umail = player.GetVar("mail");
+let umajor = player.GetVar("major");
+let uheight = player.GetVar("height");
+let uweight = player.GetVar("weight"); 
+let uage = player.GetVar("age");
+let umeasure = player.GetVar("measure");
+let uvid = player.GetVar("vid");
+let uactiv = player.GetVar("activ");
+let uwork = player.GetVar("work");
+let uslide = player.GetVar("slide");
+fetch(url,{
+  method: 'POST',
+  mode: 'no-cors',
+  cache: 'no-cache',
+  headers: {'Content-Type': 'application/json'},
+  redirect: 'follow',
+  body: JSON.stringify({id:uid,username:uname,cell:ucell,mail:umail,major:umajor,height:uheight,weight:uweight,age:uage,measure:umeasure,vid:uvid,activ:uactiv,work:uwork,slide:uslide})
+});
+}
+
+function Script3()
+{
+  // JavaScript Document// JavaScript Documentconst 
+url = "https://script.google.com/macros/s/AKfycbzrM3k8moYjvK8FLjFs2GWZ_RiYvi4T4TQiMr_QbiKgkorNcgccsSseUDmbCRPlbHoh/exec";
+const player = GetPlayer();
+let uid = player.GetVar("id"); 
+let uname = player.GetVar("username");
+let ucell = player.GetVar("cell");
+let umail = player.GetVar("mail");
+let umajor = player.GetVar("major");
+let uheight = player.GetVar("height");
+let uweight = player.GetVar("weight"); 
+let uage = player.GetVar("age");
+let umeasure = player.GetVar("measure");
+let uvid = player.GetVar("vid");
+let uactiv = player.GetVar("activ");
+let uwork = player.GetVar("work");
+let uslide = player.GetVar("slide");
+fetch(url,{
+  method: 'POST',
+  mode: 'no-cors',
+  cache: 'no-cache',
+  headers: {'Content-Type': 'application/json'},
+  redirect: 'follow',
+  body: JSON.stringify({id:uid,username:uname,cell:ucell,mail:umail,major:umajor,height:uheight,weight:uweight,age:uage,measure:umeasure,vid:uvid,activ:uactiv,work:uwork,slide:uslide})
+});
+}
+
+function Script4()
+{
+  // JavaScript Document// JavaScript Documentconst 
+url = "https://script.google.com/macros/s/AKfycbzrM3k8moYjvK8FLjFs2GWZ_RiYvi4T4TQiMr_QbiKgkorNcgccsSseUDmbCRPlbHoh/exec";
+const player = GetPlayer();
+let uid = player.GetVar("id"); 
+let uname = player.GetVar("username");
+let ucell = player.GetVar("cell");
+let umail = player.GetVar("mail");
+let umajor = player.GetVar("major");
+let uheight = player.GetVar("height");
+let uweight = player.GetVar("weight"); 
+let uage = player.GetVar("age");
+let umeasure = player.GetVar("measure");
+let uvid = player.GetVar("vid");
+let uactiv = player.GetVar("activ");
+let uwork = player.GetVar("work");
+let uslide = player.GetVar("slide");
+fetch(url,{
+  method: 'POST',
+  mode: 'no-cors',
+  cache: 'no-cache',
+  headers: {'Content-Type': 'application/json'},
+  redirect: 'follow',
+  body: JSON.stringify({id:uid,username:uname,cell:ucell,mail:umail,major:umajor,height:uheight,weight:uweight,age:uage,measure:umeasure,vid:uvid,activ:uactiv,work:uwork,slide:uslide})
+});
+}
+
+function Script5()
+{
+  // JavaScript Document// JavaScript Documentconst 
+url = "https://script.google.com/macros/s/AKfycbzrM3k8moYjvK8FLjFs2GWZ_RiYvi4T4TQiMr_QbiKgkorNcgccsSseUDmbCRPlbHoh/exec";
+const player = GetPlayer();
+let uid = player.GetVar("id"); 
+let uname = player.GetVar("username");
+let ucell = player.GetVar("cell");
+let umail = player.GetVar("mail");
+let umajor = player.GetVar("major");
+let uheight = player.GetVar("height");
+let uweight = player.GetVar("weight"); 
+let uage = player.GetVar("age");
+let umeasure = player.GetVar("measure");
+let uvid = player.GetVar("vid");
+let uactiv = player.GetVar("activ");
+let uwork = player.GetVar("work");
+let uslide = player.GetVar("slide");
+fetch(url,{
+  method: 'POST',
+  mode: 'no-cors',
+  cache: 'no-cache',
+  headers: {'Content-Type': 'application/json'},
+  redirect: 'follow',
+  body: JSON.stringify({id:uid,username:uname,cell:ucell,mail:umail,major:umajor,height:uheight,weight:uweight,age:uage,measure:umeasure,vid:uvid,activ:uactiv,work:uwork,slide:uslide})
+});
+}
+
+function Script6()
+{
+  // JavaScript Document// JavaScript Documentconst 
+url = "https://script.google.com/macros/s/AKfycbzrM3k8moYjvK8FLjFs2GWZ_RiYvi4T4TQiMr_QbiKgkorNcgccsSseUDmbCRPlbHoh/exec";
+const player = GetPlayer();
+let uid = player.GetVar("id"); 
+let uname = player.GetVar("username");
+let ucell = player.GetVar("cell");
+let umail = player.GetVar("mail");
+let umajor = player.GetVar("major");
+let uheight = player.GetVar("height");
+let uweight = player.GetVar("weight"); 
+let uage = player.GetVar("age");
+let umeasure = player.GetVar("measure");
+let uvid = player.GetVar("vid");
+let uactiv = player.GetVar("activ");
+let uwork = player.GetVar("work");
+let uslide = player.GetVar("slide");
+fetch(url,{
+  method: 'POST',
+  mode: 'no-cors',
+  cache: 'no-cache',
+  headers: {'Content-Type': 'application/json'},
+  redirect: 'follow',
+  body: JSON.stringify({id:uid,username:uname,cell:ucell,mail:umail,major:umajor,height:uheight,weight:uweight,age:uage,measure:umeasure,vid:uvid,activ:uactiv,work:uwork,slide:uslide})
+});
+}
+
+function Script7()
+{
+  // JavaScript Document// JavaScript Documentconst 
+url = "https://script.google.com/macros/s/AKfycbzrM3k8moYjvK8FLjFs2GWZ_RiYvi4T4TQiMr_QbiKgkorNcgccsSseUDmbCRPlbHoh/exec";
+const player = GetPlayer();
+let uid = player.GetVar("id"); 
+let uname = player.GetVar("username");
+let ucell = player.GetVar("cell");
+let umail = player.GetVar("mail");
+let umajor = player.GetVar("major");
+let uheight = player.GetVar("height");
+let uweight = player.GetVar("weight"); 
+let uage = player.GetVar("age");
+let umeasure = player.GetVar("measure");
+let uvid = player.GetVar("vid");
+let uactiv = player.GetVar("activ");
+let uwork = player.GetVar("work");
+let uslide = player.GetVar("slide");
+fetch(url,{
+  method: 'POST',
+  mode: 'no-cors',
+  cache: 'no-cache',
+  headers: {'Content-Type': 'application/json'},
+  redirect: 'follow',
+  body: JSON.stringify({id:uid,username:uname,cell:ucell,mail:umail,major:umajor,height:uheight,weight:uweight,age:uage,measure:umeasure,vid:uvid,activ:uactiv,work:uwork,slide:uslide})
+});
+}
+
+function Script8()
+{
+  // JavaScript Document// JavaScript Documentconst 
+url = "https://script.google.com/macros/s/AKfycbzrM3k8moYjvK8FLjFs2GWZ_RiYvi4T4TQiMr_QbiKgkorNcgccsSseUDmbCRPlbHoh/exec";
+const player = GetPlayer();
+let uid = player.GetVar("id"); 
+let uname = player.GetVar("username");
+let ucell = player.GetVar("cell");
+let umail = player.GetVar("mail");
+let umajor = player.GetVar("major");
+let uheight = player.GetVar("height");
+let uweight = player.GetVar("weight"); 
+let uage = player.GetVar("age");
+let umeasure = player.GetVar("measure");
+let uvid = player.GetVar("vid");
+let uactiv = player.GetVar("activ");
+let uwork = player.GetVar("work");
+let uslide = player.GetVar("slide");
+fetch(url,{
+  method: 'POST',
+  mode: 'no-cors',
+  cache: 'no-cache',
+  headers: {'Content-Type': 'application/json'},
+  redirect: 'follow',
+  body: JSON.stringify({id:uid,username:uname,cell:ucell,mail:umail,major:umajor,height:uheight,weight:uweight,age:uage,measure:umeasure,vid:uvid,activ:uactiv,work:uwork,slide:uslide})
+});
+}
+
+function Script9()
+{
+  // JavaScript Document// JavaScript Documentconst 
+url = "https://script.google.com/macros/s/AKfycbzrM3k8moYjvK8FLjFs2GWZ_RiYvi4T4TQiMr_QbiKgkorNcgccsSseUDmbCRPlbHoh/exec";
+const player = GetPlayer();
+let uid = player.GetVar("id"); 
+let uname = player.GetVar("username");
+let ucell = player.GetVar("cell");
+let umail = player.GetVar("mail");
+let umajor = player.GetVar("major");
+let uheight = player.GetVar("height");
+let uweight = player.GetVar("weight"); 
+let uage = player.GetVar("age");
+let umeasure = player.GetVar("measure");
+let uvid = player.GetVar("vid");
+let uactiv = player.GetVar("activ");
+let uwork = player.GetVar("work");
+let uslide = player.GetVar("slide");
+fetch(url,{
+  method: 'POST',
+  mode: 'no-cors',
+  cache: 'no-cache',
+  headers: {'Content-Type': 'application/json'},
+  redirect: 'follow',
+  body: JSON.stringify({id:uid,username:uname,cell:ucell,mail:umail,major:umajor,height:uheight,weight:uweight,age:uage,measure:umeasure,vid:uvid,activ:uactiv,work:uwork,slide:uslide})
+});
+}
+
+function Script10()
+{
+  const player = GetPlayer();
+var topTenVarName = 'topTen';
+var topTenMsg;
+var usertxt;
+var line = "------------------------------------------------------------------------------------"; 
+var url="https://script.google.com/macros/s/AKfycbzrM3k8moYjvK8FLjFs2GWZ_RiYvi4T4TQiMr_QbiKgkorNcgccsSseUDmbCRPlbHoh/exec";
+player.SetVar(topTenVarName, "انتظر");
+fetch(url)
+
+.then(res => res.json())
+.then(data => 
+{
+	topTenMsg="\n";
+for (var i = 100; i < data.content.length; i++) {
+	    topTenMsg +=((i-99)+"- ")+" الاسم:"+data.content[i][1].toString()+" الدرجة:"+data.content[i][5].toString()+" مشاهدة:"+data.content[i][9].toString()+" المجهود:"+data.content[i][11].toString()+"\n"+line+ "\n";
+	player.SetVar(topTenVarName,topTenMsg)
+}
+})
+
+}
+
+function Script11()
+{
+  // JavaScript Document// JavaScript Documentconst 
+url = "https://script.google.com/macros/s/AKfycbzrM3k8moYjvK8FLjFs2GWZ_RiYvi4T4TQiMr_QbiKgkorNcgccsSseUDmbCRPlbHoh/exec";
+const player = GetPlayer();
+let uid = player.GetVar("id"); 
+let uname = player.GetVar("username");
+let ucell = player.GetVar("cell");
+let umail = player.GetVar("mail");
+let umajor = player.GetVar("major");
+let uheight = player.GetVar("height");
+let uweight = player.GetVar("weight"); 
+let uage = player.GetVar("age");
+let umeasure = player.GetVar("measure");
+let uvid = player.GetVar("vid");
+let uactiv = player.GetVar("activ");
+let uwork = player.GetVar("work");
+let uslide = player.GetVar("slide");
+fetch(url,{
+  method: 'POST',
+  mode: 'no-cors',
+  cache: 'no-cache',
+  headers: {'Content-Type': 'application/json'},
+  redirect: 'follow',
+  body: JSON.stringify({id:uid,username:uname,cell:ucell,mail:umail,major:umajor,height:uheight,weight:uweight,age:uage,measure:umeasure,vid:uvid,activ:uactiv,work:uwork,slide:uslide})
+});
+}
+
+function Script12()
+{
+  // JavaScript Document// JavaScript Documentconst 
+url = "https://script.google.com/macros/s/AKfycbzrM3k8moYjvK8FLjFs2GWZ_RiYvi4T4TQiMr_QbiKgkorNcgccsSseUDmbCRPlbHoh/exec";
+const player = GetPlayer();
+let uid = player.GetVar("id"); 
+let uname = player.GetVar("username");
+let ucell = player.GetVar("cell");
+let umail = player.GetVar("mail");
+let umajor = player.GetVar("major");
+let uheight = player.GetVar("height");
+let uweight = player.GetVar("weight"); 
+let uage = player.GetVar("age");
+let umeasure = player.GetVar("measure");
+let uvid = player.GetVar("vid");
+let uactiv = player.GetVar("activ");
+let uwork = player.GetVar("work");
+let uslide = player.GetVar("slide");
+fetch(url,{
+  method: 'POST',
+  mode: 'no-cors',
+  cache: 'no-cache',
+  headers: {'Content-Type': 'application/json'},
+  redirect: 'follow',
+  body: JSON.stringify({id:uid,username:uname,cell:ucell,mail:umail,major:umajor,height:uheight,weight:uweight,age:uage,measure:umeasure,vid:uvid,activ:uactiv,work:uwork,slide:uslide})
+});
+}
+
+function Script13()
+{
+  // JavaScript Document// JavaScript Documentconst 
+url = "https://script.google.com/macros/s/AKfycbzrM3k8moYjvK8FLjFs2GWZ_RiYvi4T4TQiMr_QbiKgkorNcgccsSseUDmbCRPlbHoh/exec";
+const player = GetPlayer();
+let uid = player.GetVar("id"); 
+let uname = player.GetVar("username");
+let ucell = player.GetVar("cell");
+let umail = player.GetVar("mail");
+let umajor = player.GetVar("major");
+let uheight = player.GetVar("height");
+let uweight = player.GetVar("weight"); 
+let uage = player.GetVar("age");
+let umeasure = player.GetVar("measure");
+let uvid = player.GetVar("vid");
+let uactiv = player.GetVar("activ");
+let uwork = player.GetVar("work");
+let uslide = player.GetVar("slide");
+fetch(url,{
+  method: 'POST',
+  mode: 'no-cors',
+  cache: 'no-cache',
+  headers: {'Content-Type': 'application/json'},
+  redirect: 'follow',
+  body: JSON.stringify({id:uid,username:uname,cell:ucell,mail:umail,major:umajor,height:uheight,weight:uweight,age:uage,measure:umeasure,vid:uvid,activ:uactiv,work:uwork,slide:uslide})
+});
+}
+
